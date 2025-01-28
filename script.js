91a298bb360f6d3993b0a0c820eba0ef79648356
ajsinfotech233 <71782370+ajsinfotech233@users.noreply.github.com>
2025-01-28 14:44:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function acceptTerms() {
+    alert("You have accepted the terms and conditions!");
+    window.location.href = "https://www.rojgarresult.app/"; // Redirect to the internet or your actual portal
+}
